03a333280ccad0aac25314defb61a824c4cedb08
Felotxo <milenariofm@gmail.com>
2025-02-22 11:46:37 +0100

    v0.0.2

--- pagina_de_prueba_java.js
+++ pagina_de_prueba_java.js
@@ -1,5 +1,10 @@
+"use strict" 
 var name=prompt("introduce nombre", "nombre por defecto");
 var name=prompt("Que edad tienes?", 18);
 alert("esto en una alerta mamomes");
 var confirmacion = confirm ("estas en una zona excluidad de mamones, eres un mamon?");
+/me han dicho que puedo poner comentarios/
+/*y que ademas podemos poner comentarios
+en varias lineas*/
 console.log("Esto es un mensaje escrito por consola");
+
