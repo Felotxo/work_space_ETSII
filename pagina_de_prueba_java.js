0f30f870ba86c6c72c96553e7be5094729e02590
Felotxo <milenariofm@gmail.com>
2025-02-22 12:21:00 +0100

    v 1.0.0

--- pagina_de_prueba_java.js
+++ pagina_de_prueba_java.js
@@ -1,31 +1,34 @@
-"use strict" 
-var name=prompt("introduce nombre", "nombre por defecto");
-var name=prompt("Que edad tienes?", 18);
-alert("esto en una alerta mamomes");
-var confirmacion = confirm ("estas en una zona excluidad de mamones, eres un mamon?");
-//me han dicho que puedo poner comentarios
-/*y que ademas podemos poner comentarios
-en varias lineas*/
+"use strict";
+
+var name = prompt("Introduce nombre", "nombre por defecto");
+var age = prompt("¿Qué edad tienes?", 18);
+alert("Esto es una alerta, mamones");
+var confirmacion = confirm("Estás en una zona excluida de mamones, ¿eres un mamon?");
+// Me han dicho que puedo poner comentarios
+/* y que además podemos poner comentarios
+en varias líneas */
 console.log("Esto es un mensaje escrito por consola");
-//al parecer los comentarios no se quedan dentro de las dos barras.
-var num=5;
-document.write(num); //valor vale 5
-if (true) {var=10;document.write(num);//valor vale 10}
+// Al parecer los comentarios no se quedan dentro de las dos barras.
+var num = 5;
+document.write(num); // valor vale 5
+if (true) {
+    num = 10;
+    document.write(num); // valor vale 10
+}
 document.write(num);
-var num1=45;
-resultado= num1+number(num2);
-alert(parseInt(num2)+23);
-const nombre="FELOTXO";
-JavaScript es Case sensitive.
-Distingue entre mayúsculas/minúsculas
+var num1 = 45;
+var num2 = 10; // Añadí esta línea porque num2 no estaba definido
+var resultado = num1 + Number(num2);
+alert(parseInt(num2) + 23);
+const nombre = "FELOTXO";
+// JavaScript es Case sensitive.
+// Distingue entre mayúsculas/minúsculas
 var numero = 10;
 var Numero = 20;
-console.log("La variable 'numero' vale:"
-+ numero);
-console.log("La variable 'Numero' vale:"
-+ Numero);
-//Con variable numéricas podemos operar matemáticamente
+console.log("La variable 'numero' vale: " + numero);
+console.log("La variable 'Numero' vale: " + Numero);
+// Con variables numéricas podemos operar matemáticamente
 var sumando1 = 35;
 var sumando2 = 45;
-var resultado = sumando1+sumando2;
-console.log("El resultado de sumar " +sumando1 + " + " +sumando2 + " es " +resultado);
+var resultadoSuma = sumando1 + sumando2;
+console.log("El resultado de sumar " + sumando1 + " + " + sumando2 + " es " + resultadoSuma);
