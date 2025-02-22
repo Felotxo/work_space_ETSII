6a03e21a92cb2ecc4e0258df43ce02e99ceb2bf6
Felotxo <milenariofm@gmail.com>
2025-02-22 11:26:32 +0100

    v1.1.1

--- pagina_de_prueba_java.js
+++ pagina_de_prueba_java.js
@@ -2,3 +2,4 @@ var name=prompt("introduce nombre", "nombre por defecto");
 var name=prompt("Que edad tienes?", 18);
 alert("esto en una alerta mamomes");
 var confirmacion = confirm ("estas en una zona excluidad de mamones, eres un mamon?");
+console.log("Esto es un mensaje escrito por consola");
