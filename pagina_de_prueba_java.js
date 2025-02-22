98c1072efca958e9f07b9a8d10d1e3b1da28b1e5
Felotxo <milenariofm@gmail.com>
2025-02-22 12:03:50 +0100

    v 0.0.4

--- pagina_de_prueba_java.js
+++ pagina_de_prueba_java.js
@@ -3,9 +3,15 @@ var name=prompt("introduce nombre", "nombre por defecto");
 var name=prompt("Que edad tienes?", 18);
 alert("esto en una alerta mamomes");
 var confirmacion = confirm ("estas en una zona excluidad de mamones, eres un mamon?");
-/me han dicho que puedo poner comentarios/
+//me han dicho que puedo poner comentarios
 /*y que ademas podemos poner comentarios
 en varias lineas*/
 console.log("Esto es un mensaje escrito por consola");
-
-
+//al parecer los comentarios no se quedan dentro de las dos barras.
+var num=5;
+document.write(num) //valor vale 5
+if (true) {var=10;document.write(num);//valor vale 10}
+document.write(num)
+var num1=45
+resultado= num1+number(num2);
+alert(parseInt(num2)+23);
