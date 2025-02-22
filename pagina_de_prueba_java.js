992d35bf131e734d523ec3e055aeace45cb8b0fd
Felotxo <milenariofm@gmail.com>
2025-02-22 12:13:55 +0100

    v 0.0.6

--- pagina_de_prueba_java.js
+++ pagina_de_prueba_java.js
@@ -9,9 +9,23 @@ en varias lineas*/
 console.log("Esto es un mensaje escrito por consola");
 //al parecer los comentarios no se quedan dentro de las dos barras.
 var num=5;
-document.write(num) //valor vale 5
+document.write(num); //valor vale 5
 if (true) {var=10;document.write(num);//valor vale 10}
-document.write(num)
-var num1=45
+document.write(num);
+var num1=45;
 resultado= num1+number(num2);
 alert(parseInt(num2)+23);
+const nombre="FELOTXO";
+JavaScript es Case sensitive.
+Distingue entre mayúsculas/minúsculas
+var numero = 10;
+var Numero = 20;
+console.log("La variable 'numero' vale:"
++ numero);
+console.log("La variable 'Numero' vale:"
++ Numero);
+//Con variable numéricas podemos operar matemáticamente
+var sumando1 = 35;
+var sumando2 = 45;
+var resultado = sumando1+sumando2;
+console.log("El resultado de sumar " +sumando1 + " + " +sumando2 + " es " +resultado);
