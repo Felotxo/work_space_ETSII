be5c86dcdfab0ee8e3971597404f486b85af4f63
Felotxo <milenariofm@gmail.com>
2025-02-23 12:50:07 +0100

    22

--- prueba archivo Java.js
+++ prueba archivo Java.js
@@ -1,4 +1,7 @@
-var cantidad = 0;
-vacio = !cantidad; // vacio = true
-cantidad = 2;
-vacio = !cantidad; // vacio = false
+var numero1 = 10;
+var numero2 = 5;
+resultado = numero1 / numero2; //resultado = 2
+resultado = 3 + numero1; //resultado = 13
+resultado = numero2 - 4; //resultado = 1
+resultado = numero1 * numero2; //resultado = 50
+
