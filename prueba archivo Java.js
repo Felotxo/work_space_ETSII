932c2203775bcadcf2ba0462275a3fbd90e83a30
Felotxo <milenariofm@gmail.com>
2025-02-23 14:00:15 +0100

    for

--- prueba archivo Java.js
+++ prueba archivo Java.js
@@ -5,3 +5,22 @@ resultado = 3 + numero1; //resultado = 13
 resultado = numero2 - 4; //resultado = 1
 resultado = numero1 * numero2; //resultado = 50
 
+var precio = 30000;
+var dinero = prompt("Introduce cuánto dinero tienes: ");
+var edad = prompt("Introduce tu edad:");
+if ((precio < dinero) && (edad >=
+18)) {
+alert("Te puedes comprar el coche");
+} else if ((precio < dinero) && (edad
+< 18)) {
+alert("Tienes el dinero pero nola edad");
+}
+else if ((precio > dinero) && (edad>= 18))
+{alert("Tienes la edad pero no el dinero");
+}else if ((precio > dinero) && (edad <
+18)) {alert("Ni dinero ni edad");}
+for (i=0 ; i<10 ; i++){
+    document.write("En esta vuelta de bucle I vale " + i);
+    document.write("<br>");
+    }
+    document.write("Ejecución terminada.");
