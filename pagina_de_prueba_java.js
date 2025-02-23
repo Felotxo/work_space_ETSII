b67c6599c99aa241a1f9fad6e9e165ffa4297908
Felotxo <milenariofm@gmail.com>
2025-02-23 13:12:39 +0100

    if else

--- pagina_de_prueba_java.js
+++ pagina_de_prueba_java.js
@@ -9,32 +9,10 @@ var confirmacion = confirm("Estás en una zona excluida de mamones, ¿eres un ma
 en varias líneas */
 console.log("Esto es un mensaje escrito por consola");
 // Al parecer los comentarios no se quedan dentro de las dos barras.
-var num = 5;
-document.write(num); // valor vale 5
-if (true) {
-    num = 10;
-    document.write(num); // valor vale 10
+var precio=30000;
+var dinero=prompt("Introduce cuantodinero tienes: ");
+if(dinero>precio){
+alert("Te puedes comprar elcoche");
+}else{
+alert("Te vas en autobus");
 }
-document.write(num);
-var num1 = 45;
-var num2 = 10; // Añadí esta línea porque num2 no estaba definido
-var resultado = num1 + Number(num2);
-alert(parseInt(num2) + 23);
-const nombre = "FELOTXO";
-// JavaScript es Case sensitive.
-// Distingue entre mayúsculas/minúsculas
-var numero = 10;
-var Numero = 20;
-console.log("La variable 'numero' vale: " + numero);
-console.log("La variable 'Numero' vale: " + Numero);
-// Con variables numéricas podemos operar matemáticamente
-var sumando1 = 35;
-var sumando2 = 45;
-var resultadoSuma = sumando1 + sumando2;
-console.log("El resultado de sumar " + sumando1 + " + " + sumando2 + " es " + resultadoSuma);
-var texto="Esto es el texto";
-var busqueda= texto.indexOf("texto");doument.write(busqueda);
-var busqueda= texto.match(/texto/gi) // con esto nos devuelve todos los resultados que tengamos de lo que buscas
-var numero78 = 9;
-numero78++;
-alert(numero78);
