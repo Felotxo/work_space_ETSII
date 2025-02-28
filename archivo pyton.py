96874e051b93ad1b862956cb65d5245d04a377d5
Felotxo <milenariofm@gmail.com>
2025-02-28 11:27:41 +0100

    a

--- archivo pyton.py
+++ archivo pyton.py
@@ -1,4 +1,13 @@
-l = list()
+l = list()      #Creamos una lista vacia
+texto = input("introduce un numero entero: ")
+if texto.isnumeric(): #comprueba si son numeros 
+    num = int(texto)
+    l.append(num)  
+    print("numero entero introducido " + str(num) + " añadido a la lista")
+else:
+    print("no has introducido ningun numero entero")
+    
+
 
 
 
