73c3726aafd17b3c5eaea1c15d5b694e36b5eeaa
Felotxo <milenariofm@gmail.com>
2025-03-05 10:14:27 +0100

    notas

--- bucle for.py
+++ bucle for.py
@@ -1,10 +1,11 @@
-letras = list("abcdefghijklmnopqrstuvwxyz")
-l1 = letras[:8]
-l2 = letras[8:16]
-l3 = letras[16:]
-random.shuffle(l1)
-random.shuffle(l2)
-random.shuffle(l3)
-for a, b, c, in zip(l1, l2, l3):
-    print(a + b + c, end=" ")
+n_nota = float(input("introduzca nota:"))
+
+if n_nota>5:
+    calificacion="aprobado"
+else:
+    calificacion="suspenso"
+print(calificacion)
+
+
+
 
