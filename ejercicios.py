a640de895dde46e54832489ab64702710cbb9a9a
Felotxo <milenariofm@gmail.com>
2025-03-06 10:33:43 +0100

    ole

---
+++ ejercicios.py
@@ -0,0 +1,8 @@
+x = int(input("introduce un numero a: "))
+y = int(input("introduce un numero b: "))
+if x < y:
+        print("el numero menor es: ", x)
+elif x == y:
+        print("a y b son iguales")
+else:
+        print("el numero menor es: ", y)
